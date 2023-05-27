88ce692e7511fac732a5327d9f01b95efeef95ed
ofurum <olufemioluwatobi1996@gmail.com>
2023-05-27 12:20:16 +0100

    improve comments

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -1,13 +1,21 @@
-"use client";
-
 import React, { useState, useEffect } from "react";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
+/**
+ *
+ * StyledComponentsRegistry component is responsible for managing the integration of styled-components library
+ * with server-side rendering (SSR) and client-side rendering (CSR) in a React application.
+ */
 export default function StyledComponentsRegistry({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  /**
+   * useState hook is used to initialize the styledComponentsStyleSheet state.
+   * In the server-side environment, it creates a new ServerStyleSheet instance.
+   * In the client-side environment, it initializes the state to null.
+   */
   const [styledComponentsStyleSheet] = useState(() => {
     if (typeof window === "undefined") {
       return new ServerStyleSheet();
@@ -16,12 +24,21 @@ export default function StyledComponentsRegistry({
     }
   });
 
+  /**
+   * The useEffect hook is used to perform cleanup when the component is unmounted.
+   * It clears the style tag associated with the ServerStyleSheet instance, if available.
+   */
   useEffect(() => {
     if (styledComponentsStyleSheet) {
       return () => styledComponentsStyleSheet.instance.clearTag();
     }
   }, [styledComponentsStyleSheet]);
 
+  /**
+   * In the client-side environment (typeof window !== "undefined") and if the styledComponentsStyleSheet is available,
+   * the component wraps the children with the StyleSheetManager component from styled-components.
+   * It provides the style sheet instance to ensure proper style application during rendering.
+   */
   if (typeof window !== "undefined" && styledComponentsStyleSheet) {
     return (
       <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
@@ -30,5 +47,9 @@ export default function StyledComponentsRegistry({
     );
   }
 
+  /**
+   * If the component is being rendered in a server-side environment or the styledComponentsStyleSheet is not available,
+   * it simply renders the children without any additional wrapping.
+   */
   return <>{children}</>;
 }
